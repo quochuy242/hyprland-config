ab0317054b51776d27c710e03a781d5e7ada6b4c
quochuy242 <truongquochuy.240203@gmail.com>
2025-04-23 19:50:28 +0700

    update

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -15,6 +15,15 @@ fi
 # Using swww to change the wallpaper
 if [ -n "$WALLPAPER" ]; then
   swww img "$WALLPAPER" --transition-fps 255 --transition-type outer --transition-duration 0.8
+  
+  # Generate colorscheme with pywal
+  wal -i "$WALLPAPER" --backend auto --saturate 0.5
+  
+  # Reload any app that uses the colors if needed
+  # Example: reload waybar, rofi, kitty, etc.
+  # killall -SIGUSR1 waybar
+  # (you can add more here depending on your setup)
+  
 else
   echo "No wallpapers found in $WALLPAPER_DIR"
 fi
--- waybar/README.md
+++
@@ -1,3 +0,0 @@
-- Modified from: https://github.com/Ruixi-rebirth/sway-dotfiles/blob/main/waybar/
-- Author: Ruixi-rebirth
-- Retrived on 19/03/2025
